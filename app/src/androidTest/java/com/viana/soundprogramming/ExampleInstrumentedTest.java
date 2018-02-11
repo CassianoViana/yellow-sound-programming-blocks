276c4807659b09f49fa1cee31e27b128af70eb6e
Cassiano Viana <cassiano.pvianna@gmail.com>
2018-02-11 16:36:25 -0200

    Mudança nome projeto

--- app/src/androidTest/java/com/viana/soundprogramming/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/viana/soundprogramming/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.viana.testetopcode", appContext.getPackageName());
+        assertEquals("com.viana.SoundProgramming", appContext.getPackageName());
     }
 }
