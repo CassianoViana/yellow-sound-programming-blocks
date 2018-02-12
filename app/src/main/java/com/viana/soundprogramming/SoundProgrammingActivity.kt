1ef56c814ef78a2f524b4b5387723fc07ec7d09f
Cassiano Viana <cassiano.pvianna@gmail.com>
2018-02-11 23:27:46 -0200

    Funcionando leitura e desenho dos topcodes com classe que extends de View

--- app/src/main/java/com/viana/soundprogramming/SoundProgrammingActivity.kt
+++ app/src/main/java/com/viana/soundprogramming/SoundProgrammingActivity.kt
@@ -41,12 +41,25 @@ class SoundProgrammingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        hideSystemUI(window.decorView)
         setContentView(R.layout.activity_sound_programming)
         scanner = Scanner()
         bitmapReader = BitmapReader(this)
         cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
-        surfaceView.setAspectRatio(4, 6)
+//        surfaceView.setAspectRatio(6, 3)
     }
+
+    private fun hideSystemUI(mDecorView :View) {
+    // Set the IMMERSIVE flag.
+    // Set the content to appear under the system bars so that the content
+    // doesn't resize when the system bars hide and show.
+        mDecorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                or View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                or View.SYSTEM_UI_FLAG_IMMERSIVE)
+}
 
     fun start(view: View) {
         openCamera()
@@ -191,10 +204,9 @@ class SoundProgrammingActivity : AppCompatActivity() {
     }
 
     private fun paintTopcodes() {
-        frameLayout.removeAllViews()
-        topCodes?.forEach {
-            val topCodeView = TopCodeView(it, this)
-            frameLayout.addView(topCodeView)
+        topCodes?.let {
+            topCodesView.topCodes = it
         }
+        topCodesView.invalidate()
     }
 }
